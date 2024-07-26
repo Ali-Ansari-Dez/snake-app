51e3b61b58f1df46cb6f45bcd9b033541ab6baf8
MightylilAl <alee.ansari@gmail.com>
2024-07-26 22:13:01 +0330

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useRef, ChangeEvent } from "react";
-import logo from "./logo.svg";
+import React, { useState, useRef, ChangeEvent, useEffect } from "react";
+import useInterval from "./useInterval";
 import "./App.css";
-
 
 // type CanvasProps = React.DetailedHTMLProps<
 //   React.CanvasHTMLAttributes<HTMLCanvasElement>,
@@ -13,28 +12,98 @@ const gridSize: number = 30;
 const columns: number = 30;
 const rows: number = 20;
 
-
+const initialSnake = [ [ 15, 10 ], [ 15, 11 ],[15,12] ]
+const initialApple = [[4,4]]
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [difValue, setDifValue] = useState(100); //to change difficulty later
-  const [Snake,setSnake] = useState()
-  const [apple,setApple] = useState()
-  const [snakeDirection,setSnakeDirection] = useState();
-  const [gameOver,setGameOver] = useState()
+  const [difValue, setDifValue] = useState(500); //to change difficulty later
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [snakeDirection, setSnakeDirection] = useState([0, -1]);
+  const [gameOver, setGameOver] = useState();
+
+  useInterval(() => runGame(), difValue)
+  
+  
 
 
-  const moveSnake = (e: React.KeyboardEvent<HTMLDivElement>) => {};
 
 
-
-  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
-    let value = parseInt(event.target.value)
-    setDifValue(value)
+  const randomApplePlacement = () => {
+    let newApple = initialApple.map(([x,y])=>[
+      Math.floor(Math.random() * x),
+      Math.floor(Math.random() * y)
+    ])
+    setApple(newApple)
   }
 
+  
+
+  const runGame = () => {
+    const newSnake = [ ...snake ]
+    const newSnakeHead = [ newSnake[0][0] + snakeDirection[0], newSnake[0][1] + snakeDirection[1] ]
+    newSnake.unshift(newSnakeHead)
+    setSnake(newSnake)
+  }
+
+  
+  const changeDirection = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    switch (event.key) {
+      case "A":
+      case "ArrowLeft":
+        setSnakeDirection([-1, 0]);
+        break;
+      case "W":
+      case "ArrowUp":
+        setSnakeDirection([0, -1]);
+        break;
+      case "D":
+      case "ArrowRight":
+        setSnakeDirection([1, 0]);
+        break;
+      case "S":
+      case "ArrowDown":
+        setSnakeDirection([0, 1]);
+        break;
+    }
+  };
+
+  const handleRangeChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    let value = parseInt(event.target.value);
+    setDifValue(value);
+  };
+  const play =() => {
+		setSnake(initialSnake)
+		setApple(initialApple)
+		setSnakeDirection([ 0, -1 ])
+    runGame()
+	}
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current
+      const contex = canvas.getContext("2d")
+      if (contex) {
+        contex.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        contex.fillStyle ="red"
+        contex.fillRect(apple[0][0]*gridSize,apple[0][1]*gridSize,gridSize,gridSize)
+
+        contex.fillStyle = "#a3d001"
+        contex.strokeStyle = "#000"
+        snake.forEach(([ x, y ]) => contex.fillRect(x * gridSize , y * gridSize, gridSize, gridSize))
+        snake.forEach(([x,y]) =>contex.strokeRect(x * gridSize , y * gridSize, gridSize, gridSize))
+      }
+    }
+
+    // if (divRef.current) {
+    //   divRef.current.focus();
+    // }
+  }, [snake, apple, gameOver]);
+
+  
   return (
-    <div className="App" role="button" onKeyDown={(e) => moveSnake(e)}>
+    <div className="App" role="button" tabIndex={0} onKeyDown={(event) => changeDirection(event)}>
       <h1>my Snake game app thingy</h1>
       <div className="difficultyAndScore">
         <form>
@@ -47,16 +116,18 @@ function App() {
             max="500"
             step="50"
             defaultValue={difValue}
-            onChange={handleChange}
+            onChange={handleRangeChange}
           />
         </form>
+        <button type="button" onClick={play}>play</button>
       </div>
       <canvas
-      ref={canvasRef}
+        ref={canvasRef}
         id="myCanvas"
         width={gridSize * columns}
         height={gridSize * rows}
       ></canvas>
+      
     </div>
   );
 }
